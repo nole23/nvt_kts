ab34f2efb6e6b8d828cb01c30c9176a800c4119e
Krisina <kristipomo@gmail.com>
2017-01-16 19:01:25 +0100

    Ispravak gresaka

--- Konstrukcija/src/test/java/com/konstrukcija/test/konstante/KorisnikKonstante.java
+++ Konstrukcija/src/test/java/com/konstrukcija/test/konstante/KorisnikKonstante.java
@@ -8,5 +8,17 @@ public class KorisnikKonstante {
 	public static final String EMAIL = "nole0223@gmail.com";
 	public static final String PASSWORD = "123";
 	public static final String USERNAME = "nole";
-	
+	public static final String PASSWORD_THIRD = "321";
+	public static final String FIRST_NAME_THIRD = "elon";
+    public static final int DB_COUNT = 10;
+    public static final Long ADMIN_ID = 1l;
+    public static final String ADMIN_USERNAME = "admin";
+    public static final String ADMIN_EMAIL = "admin";
+    public static final String ADMIN_F_NAME = "admin";
+	public static final String ADMIN_L_NAME = "admin";
+	public static final boolean VERIFIED = false;
+	public static final String USERNAME_SECOND = "nole";
+	public static final String PASSWORD_SECOND = "123";
+	public static final String EMAIL_SECOND = "nole0223@gmail.com";
+	public static final String VERIFY_CODE = "89c631c2-c7bb-4483-ab36-4b8d4b0b8960";
 }
--- Konstrukcija/src/test/java/com/konstrukcija/test/konstante/NekretnineKonstante.java
+++ Konstrukcija/src/test/java/com/konstrukcija/test/konstante/NekretnineKonstante.java
@@ -3,15 +3,14 @@ package com.konstrukcija.test.konstante;
 public class NekretnineKonstante {
 
 	public static final int NEKRETNINA_ID = 1;
-	public static final String NAZIV_NEKRETNINE = "Nekretnina";
-	public static final Double CENA = 123.5;
-	public static final Double POVRSINA = 12.3;
-	public static final String SOBNOST = "Da";
-	public static final String STANJE_OBJEKTA = "Dobro";
-	public static final String GREJANJE = "Da";
-	public static final String SPRATOVA = "Osam";
-	public static final String STANJE = "Dobro";
-	public static final String SPRAT = "Treci";
-	public static final String OPIS = "Lep";
+	public static final String NAZIV_NEKRETNINE = "Novica";
+	public static final Double CENA = 35000.00;
+	public static final Double POVRSINA = 55.00;
+	public static final String SOBRNOS = "3";
+	public static final String STANJE_OBJEKTA = "novo";
+	public static final String GREJANJE = "parno";
+	public static final String SPRATOVA = "2";
+	public static final String SPRAT = "";
+	public static final String OPIS = "novo";
 	
 }
