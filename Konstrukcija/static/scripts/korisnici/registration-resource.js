da9dacf8249ee64b79d2f7347eef70d35444ea59
Novica Nikolic <nole0223@gmail.com>
2017-01-23 17:40:33 +0100

    Angular resource za cuvanje atributa potrebnih za kreiranje nekretnine issues #19

--- Konstrukcija/static/scripts/korisnici/registration-resource.js
+++ Konstrukcija/static/scripts/korisnici/registration-resource.js
@@ -11,7 +11,10 @@
         var service = {};
 
         service.saveNewKorisnik = saveNewKorisnik;
-        
+        service.addNewEstate = addNewEstate;
+        service.addOpremljenost = addOpremljenost;
+        service.addOLocation = addOLocation;
+        service.publishEstate = publishEstate;
 
         return service;
 
@@ -22,5 +25,33 @@
                     callback(response)
                 });
         }
+        
+        function addNewEstate(idKategorija, nekretnina, idKompanija, idKorisnik, callBack) {
+        	var link = 'http://localhost:8080/api/nekretnineee/add/'+idKategorija+'/'+idKorisnik+'/'+idKompanija;
+        	$http.post(link, nekretnina).success(function (response) {
+        		callBack(response);
+        	});
+        }
+        
+        function addOpremljenost(opremljenost, id, callBack) {
+        	var link = 'http://localhost:8080/api/nekretnineee/tehnicka/opremljenost/'+id;
+        	$http.post(link, opremljenost).success(function (response) {
+        		callBack(response);
+        	})
+        }
+        
+        function addOLocation(location, id, callBack) {
+        	var link = 'http://localhost:8080/api/nekretnineee/lokacija/'+id;
+        	$http.post(link, location).success(function (response) {
+        		callBack(response);
+        	})
+        }
+        
+        function publishEstate(id, datum, callBack) {
+			var link = 'http://localhost:8080/api/oglas/add/'+id;
+			$http.post(link, datum).success(function (response) {
+        		callBack(response);
+        	})
+		}
     }
 })();
